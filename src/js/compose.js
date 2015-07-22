7fa65f6614e40d507e5089b81c37dc8cbb79caeb
Patrick Brooks <pat.w.brooks@gmail.com>
2015-07-22 00:44:08 -0700

    end of night

---
+++ src/js/compose.js
@@ -0,0 +1,9 @@
+module.exports = function compose () {
+    var fns = arguments;
+    return function (result) {
+        for (var i = fns.length - 1; i > -1; i--) {
+            result = fns[i].call(this, result);
+        }
+        return result;
+    };
+};
--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,53 @@
-var $ = require("jquery");
-var R = require("ramda");
 require("./autoCurry.js")();
+var compose = require("./compose.js");
+var songs = require("./songs.js");
+var lunr = require("lunr");
 
+// song pages
 var makeEl = function makeEl (tagName) {
-    return $("<" + tagName + "></" + tagName + ">");
+    return document.createElement(tagName);
 };
-var setAttr = function setAttr (attributeName, el, value) {
-    el.attr(attributeName, value);
+
+var makeImg = function makeImg (src) {
+    var img = makeEl("img");
+    img.src = src;
+    return img;
+};
+
+var makeSrc = function makeSrc (number) {
+    return "/public/img/fakebook-" + number + ".png";
+};
+
+var appendEl = function appendEl (parent, child) {
+    parent.appendChild(child);
+    return child;
+}.autoCurry();
+
+var addClass = function addClass (name, el) {
+    if (el.className.indexOf(name) < 0) {
+        if (el.className) {
+            el.className += " ";
+        }
+        el.className += name;
+    }
     return el;
 }.autoCurry();
 
-window.makeImg = setAttr("src")(makeEl("img"));
+var addPage = compose(appendEl(document.body), 
+                      addClass("song-page"), 
+                      makeImg, 
+                      makeSrc);
+
+
+// search
+var idx = lunr(function () {
+    this.field("title");
+});
+Object.keys(songs).map(function (key) {
+    console.log(key);
+    idx.add({
+        title: key,
+        id: key
+    });
+});
+window.idx = idx;
